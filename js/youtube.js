a289a01fe48c6710737a5848dd5719c371285340
Carlos Delgado <carlos.andres.delgado@correounivalle.edu.co>
2024-08-18 23:04:24 -0500

    Fix error

--- js/youtube.js
+++ js/youtube.js
@@ -2,7 +2,6 @@ function onYouTubeIframeAPIReady() {
   new YT.Player('player', {
     height: '1080',
     width: '1920',
-    videoId: 'VIDEO_ID',
     events: {
       'onReady': function(event) {
         event.target.setPlaybackQuality('hd1080');
