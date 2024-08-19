28a9d643af836f046fec6fd8f020d963c6f1bc91
Joshua Triana <joshua.triana@correounivalle.edu.co>
2024-08-18 19:21:11 -0700

    try width3

--- js/clase1.1.js
+++ js/clase1.1.js
@@ -1,15 +1,13 @@
-const {
-  H5P
-} = H5PStandalone;
+const { H5P } = H5PStandalone;
 
 async function setup() {
-  await new H5P(document.getElementById('h5p-container-1'),
-          {
-            h5pJsonPath: 'recursos/h5p/clase1.1',
-            frameJs: '/dist/frame.bundle.js',
-            frameCss: '/dist/styles/h5p.css'
-            embedCode:'<iframe width=":w" height=":h" src="https://www.youtube.com/watch?v=wXhH4BjuKI8" frameBorder="0" scrolling="no" styles="width:50%"></iframe>',
-          });
+  await new H5P(document.getElementById("h5p-container-1"), {
+    h5pJsonPath: "recursos/h5p/clase1.1",
+    frameJs: "/dist/frame.bundle.js",
+    frameCss: "/dist/styles/h5p.css",
+    embedCode:
+      '<iframe width=":w" height=":h" src="https://www.youtube.com/watch?v=wXhH4BjuKI8" frameBorder="0" scrolling="no" styles="width:50%"></iframe>',
+  });
 }
 
 setup();
