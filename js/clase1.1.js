889750421575b95db64dd5fec7f9bb4c263bd2b9
Carlos Delgado <carlos.andres.delgado@correounivalle.edu.co>
2024-08-19 09:43:59 -0500

    Fix active tab

--- js/clase1.1.js
+++ js/clase1.1.js
@@ -1,23 +1,32 @@
 const { H5P } = H5PStandalone;
 
 document.addEventListener('DOMContentLoaded', function() {
+  const firstTab = document.querySelector('.nav-link.active');
+  if (firstTab) {
+      loadH5PContent(firstTab);
+  }
+
   const tabs = document.querySelectorAll('.nav-link');
   tabs.forEach(tab => {
-      tab.addEventListener('shown.bs.tab', async (event) => {
-          const targetId = event.target.getAttribute('data-bs-target');
-          const containerId = targetId.replace('#content-', 'h5p-container-');
-          const index = targetId.replace('#content-', '');
-          const container = document.getElementById(containerId);
-
-          // Check if H5P has already been loaded for this tab
-          if (!container.classList.contains('h5p-loaded')) {
-              await new H5P(container, {
-                  h5pJsonPath: `recursos/h5p/clase1.${index}`,
-                  frameJs: "/dist/frame.bundle.js",
-                  frameCss: "/dist/styles/h5p.css",
-              });
-              container.classList.add('h5p-loaded'); // Mark it as loaded
-          }
+      tab.addEventListener('shown.bs.tab', function (event) {
+          loadH5PContent(event.target);
       });
   });
 });
+
+function loadH5PContent(tab) {
+  const targetId = tab.getAttribute('data-bs-target');
+  const containerId = targetId.replace('#content-', 'h5p-container-');
+  const index = targetId.replace('#content-', '');
+  const container = document.getElementById(containerId);
+
+  if (!container.classList.contains('h5p-loaded')) {
+      new H5P(container, {
+          h5pJsonPath: `recursos/h5p/clase1.${index}`,
+          frameJs: "/dist/frame.bundle.js",
+          frameCss: "/dist/styles/h5p.css",
+      }).then(() => {
+          container.classList.add('h5p-loaded'); // Mark it as loaded
+      });
+  }
+}
