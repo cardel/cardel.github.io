728fdc559506149c18bb025931373a69b08d9a31
Carlos Delgado <carlos.andres.delgado@correounivalle.edu.co>
2024-08-19 09:32:14 -0500

    async

--- js/clase1.1.js
+++ js/clase1.1.js
@@ -1,9 +1,13 @@
 const { H5P } = H5PStandalone;
 
-for (let i = 1; i <= 10; i++) {
-  await new H5P(document.getElementById("h5p-container-" + i), {
-    h5pJsonPath: "recursos/h5p/clase1." + i,
-    frameJs: "/dist/frame.bundle.js",
-    frameCss: "/dist/styles/h5p.css",
-  });
+async function loadH5PInstances() {
+  for (let i = 1; i <= 10; i++) {
+    await new H5P(document.getElementById("h5p-container-" + i), {
+      h5pJsonPath: "recursos/h5p/clase1." + i,
+      frameJs: "/dist/frame.bundle.js",
+      frameCss: "/dist/styles/h5p.css",
+    });
+  }
 }
+
+loadH5PInstances();
